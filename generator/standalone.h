be810ed26ec5c917d1645456b4aec7be93a4be8b
vam <vam@google.com>
2019-04-16 14:24:40 -0700

    fix clang-format error

--- generator/standalone.h
+++ generator/standalone.h
@@ -1,5 +1,5 @@
-#ifndef GENERATOR_STANDALONE_H_
-#define GENERATOR_STANDALONE_H_
+#ifndef GAPIC_GENERATOR_CPP_GENERATOR_STANDALONE_H_
+#define GAPIC_GENERATOR_CPP_GENERATOR_STANDALONE_H_
 
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/compiler/plugin.pb.h>
@@ -8,16 +8,14 @@ namespace google {
 namespace api {
 namespace codegen {
 
-int StandaloneMain(int argc,
-                   const char* const argv[],
+int StandaloneMain(int argc, const char* const argv[],
                    google::protobuf::compiler::CodeGenerator* generator);
 
 int StandaloneMain(const std::vector<std::string>& descriptors,
-                   const std::string& package,
-                   const std::string& output,
+                   const std::string& package, const std::string& output,
                    google::protobuf::compiler::CodeGenerator* generator);
 
-}  // namespace google
-}  // namespace api
 }  // namespace codegen
-#endif  // GENERATOR_STANDALONE_H_
+}  // namespace api
+}  // namespace google
+#endif  // GAPIC_GENERATOR_CPP_GENERATOR_STANDALONE_H_
